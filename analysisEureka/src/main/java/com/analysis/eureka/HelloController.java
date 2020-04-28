f5a9b3ffd0a15cda2f840061548d3b878b6b003e
zajjlpff <zeng.xianlian@ztesoft.com>
2020-04-28 17:11:28 +0800

    11

---
+++ analysisEureka/src/main/java/com/analysis/eureka/HelloController.java
@@ -0,0 +1,25 @@
+package com.analysis.eureka;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * @Auther: zt
+ * @Date: 2020/03/19
+ * @Description:demand module
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    @GetMapping("/sayHello")
+    @ResponseBody
+    public Object sayHello(){
+        return "hello 111111";
+    }
+
+
+
+}
